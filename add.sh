7e2561d2e047143f0ef890ee4e0663dc6c3a8312
magnetar-x <sharmaaaditya1412@gmail.com>
2026-08-28 17:02:00 +0530

    ADD Toggler Key-Bind

---
+++ add.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "o.launch_on_start('omarchy-shell shell toggle my.scifi.hud '{}'')" >> ~/.config/hypr/autostart.lua
+echo "o.bind('SUPER + H','BridgeControltoggler','~/.config/omarchy/plugins/BridgeControl/toggler.sh')" >> ~/.config/hypr/bindings.lua
